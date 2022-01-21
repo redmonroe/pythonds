90417c786f65541f37fec4519d9502e4c1d6249f
joe <joewalsh@vistulaofindiana.com>
2022-01-21 17:51:20 -0500

    trying to introspect connectors etc

--- chapter1inheritance.py
+++ chapter1inheritance.py
@@ -47,7 +47,16 @@ class UnaryGate(LogicGate):
         self.pin = None
 
     def get_pin(self):
-        return(int(input("Enter Pin input for gate"+ self.get_label()+ "------>")))
+        if self.pin == None:
+            return(int(input("Enter Pin input for gate"+ self.get_label()+ "------>")))
+        else:
+            return self.pin.get_from().get_output()
+
+    def set_next_pin(self,source):
+        if self.pin == None:
+            self.pin = source
+        else:
+            print("Cannot Connect: NO EMPTY PINS on this gate")
 
 class AndGate(BinaryGate):
     
@@ -80,11 +89,11 @@ class NotGate(UnaryGate):
         super(UnaryGate, self).__init__(n)
 
     def perform_gate_logic(self):
-        a = self.get_pin()
-        if a==0:
-            return 1
-        else:
-            return 0
+        if self.get_pin:
+            if a==0:
+                return 1
+            else:
+                return 0
 
 class Connector():
     def __init__(self, fgate, tgate):
@@ -99,5 +108,22 @@ class Connector():
     def get_to(self):
         return self.togate
 
-g1 = NotGate("G1")
-print(g1.get_output())
+# g1 = NotGate("G1")
+# print(g1.get_output())
+def main():
+    g1 = AndGate('G1')
+    g2 = AndGate('G2')
+    g3 = OrGate('G3')
+    g4 = NotGate('G4')
+    c1 = Connector(g1, g2)
+    c2 = Connector(g2, g3)
+    c3 = Connector(g3, g4)
+    print(g4.get_output())
+
+def what_is_going_on_here():
+    g1 = AndGate("X1")
+    print(g1.get_output())
+    g2 = AndGate("X2")
+    
+
+what_is_going_on_here()
